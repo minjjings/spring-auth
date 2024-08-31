9781233e00a5c8ea7b4f585c5f27fec31096c10c
ROHMINKYUNG <142300528+minjjing0@users.noreply.github.com>
2024-08-31 17:07:04 +0900

    로그인 구현

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.sparta.springauth.controller;
 
+import com.sparta.springauth.dto.LoginRequestDto;
 import com.sparta.springauth.dto.SignupRequestDto;
-import com.sparta.springauth.entity.User;
+
 import com.sparta.springauth.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -34,5 +36,17 @@ public class UserController {
     public String signup(SignupRequestDto requestDto) {
         userService.signup(requestDto);
         return "signup";
+    }
+
+    @PostMapping("/user/login")
+    public String login(LoginRequestDto loginRequestDto,
+                        HttpServletResponse res) {
+        try {
+            userService.login(loginRequestDto,res);
+        } catch (Exception e) {
+            return "redirect:/api/user/login-page?error";
+        }
+
+        return "redirect:/";
     }
 }
