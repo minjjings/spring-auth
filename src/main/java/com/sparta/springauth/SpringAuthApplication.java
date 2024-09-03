529395cfb699d76d9592ace1cb9b7e0e22bbd957
ROHMINKYUNG <142300528+minjjing0@users.noreply.github.com>
2024-09-03 17:57:43 +0900

    Spring Security 로그인

--- src/main/java/com/sparta/springauth/SpringAuthApplication.java
+++ src/main/java/com/sparta/springauth/SpringAuthApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 // Spring Security 인증 기능 제외
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class SpringAuthApplication {
 
     public static void main(String[] args) {
